2082a9033852c14ebf670c88ca1c9192f8b066d8
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 12:21:29 +0000

    Add error prevention for env file

--- issue_reporting/settings.py
+++ issue_reporting/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+# Prevent error when hidden env.py is not found
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
