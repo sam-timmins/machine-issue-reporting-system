8caf7fcbeac2c4a3baad906cc3b39d80b0f6dd12
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 12:22:12 +0000

    Change secret key

--- issue_reporting/settings.py
+++ issue_reporting/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k0z6)@iuvec_cnlvx%drz$k%dp%(usz5l=r%8u6_8sy2f$mjp^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
