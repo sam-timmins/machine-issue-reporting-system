b83094944a9e4315fb1af9acaf70148ae9f8ce3f
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 15:38:11 +0000

    Add allauth to installed apps

--- issue_reporting/settings.py
+++ issue_reporting/settings.py
@@ -43,11 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'issue',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
