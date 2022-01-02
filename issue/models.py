cab37ac7aa4aeec2e16b8430e9afaa6bb52ed6fc
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 14:23:30 +0000

    Add Machine model

--- issue/models.py
+++ issue/models.py
@@ -6,5 +6,27 @@ from django.contrib.auth.models import AbstractUser
 STATUS = ((0, 'Faulty'), (1, 'Working'))
 
 
+class Machine(models.Model):
+    """
+    Recording machine information
+    """
+
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=1)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
 class User(AbstractUser):
+    """
+    Customizable User model, inheriting from Django's AbstractUser
+    """
     pass
