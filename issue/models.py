4d4798b7c68f63465055a310dffc0f8b3ca17d21
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 14:21:02 +0000

    Add STATUS variable for Machine model

--- issue/models.py
+++ issue/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+# Status of the machine in Machine
+STATUS = ((0, 'Faulty'), (1, 'Working'))
+
+
 class User(AbstractUser):
     pass
-
