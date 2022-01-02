125d99d872ec4c0368297b71ac3c18833f665102
sam-timmins <sam.timmins1@gmail.com>
2022-01-02 21:55:49 +0000

    Add auto increment of year for footer

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,15 @@
 $("#social-links-button").click(function(){
     $("#social-links").toggle("fade", 200)
 })
+
+
+// Auto increment of the year in footer
+
+const copyrightYearRef = document.querySelector('#copyright-year')
+
+const getFullYear = () => {
+    let year = new Date().getFullYear();
+    copyrightYearRef.innerHTML = year;
+}
+
+getFullYear()
