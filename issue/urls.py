e279d545bd66ffe26500c8c4aa07f1b6b32a4910
sam-timmins <sam.timmins1@gmail.com>
2022-01-03 20:13:20 +0000

    Add url for delete machine

--- issue/urls.py
+++ issue/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.Homepage.as_view(), name="home"),
     path("dashboard/", views.Dashboard.as_view(), name="dashboard"),
     path("dashboard/create/machine/", views.CreateMachine.as_view(), name="create-machine"),
+    path("dashboard/delete/machine/<int:pk>", views.delete_machine, name="delete-machine"),
 ]
