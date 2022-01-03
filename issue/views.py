f0995dcdd0c96a9a0257b317051a7a583f1fb27f
sam-timmins <sam.timmins1@gmail.com>
2022-01-03 11:50:56 +0000

    Add view for dashboard

--- issue/views.py
+++ issue/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import generic
+
+from .models import Machine
 
 
 class Homepage(TemplateView):
@@ -7,3 +10,12 @@ class Homepage(TemplateView):
     Views the homepage
     """
     template_name = 'index.html'
+
+
+class Dashboard(generic.ListView):
+    """
+    View for the machines model ordered by the status, then
+    name
+    """
+    queryset = Machine.objects.all().order_by('status', 'name')
+    template_name = 'pages/dashboard.html'
