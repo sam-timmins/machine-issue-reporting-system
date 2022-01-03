1aec6002c3be0553ab0bf2b2c8325b6ece6e1ac3
sam-timmins <sam.timmins1@gmail.com>
2022-01-03 16:31:23 +0000

    Add textarea size change depending on screen width

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,39 @@
 // Toggle social links container
-$("#social-links-button").click(function(){
+$("#social-links-button").click(function () {
     $("#social-links").toggle("fade", 200)
 })
+
+
+// Sets the width of textareas depending on screen size 
+if ($(window).width() >= 768) {
+    $(document).ready(function () {
+        $("textarea").attr({
+            "rows": "10",
+            "cols": "55"
+        });
+    });
+} else if ($(window).width() >= 576) {
+    $(document).ready(function () {
+        $("textarea").attr({
+            "rows": "8",
+            "cols": "50"
+        });
+    });
+} else if ($(window).width() >= 400) {
+    $(document).ready(function () {
+        $("textarea").attr({
+            "rows": "5",
+            "cols": "35"
+        });
+    });
+} else {
+    $(document).ready(function () {
+        $("textarea").attr({
+            "rows": "5",
+            "cols": "30"
+        });
+    });
+}
 
 
 // Auto increment of the year in footer
