c95e661a988678ede68b5118f73ea22931afac89
sam-timmins <sam.timmins1@gmail.com>
2022-01-08 15:49:39 +0000

    Remove textarea resize

--- static/js/script.js
+++ static/js/script.js
@@ -4,38 +4,6 @@ $("#social-links-button").click(function () {
 })
 
 
-// Sets the width of textareas depending on screen size 
-if ($(window).width() >= 768) {
-    $(document).ready(function () {
-        $("textarea").attr({
-            "rows": "10",
-            "cols": "55"
-        });
-    });
-} else if ($(window).width() >= 576) {
-    $(document).ready(function () {
-        $("textarea").attr({
-            "rows": "8",
-            "cols": "50"
-        });
-    });
-} else if ($(window).width() >= 400) {
-    $(document).ready(function () {
-        $("textarea").attr({
-            "rows": "5",
-            "cols": "35"
-        });
-    });
-} else {
-    $(document).ready(function () {
-        $("textarea").attr({
-            "rows": "5",
-            "cols": "30"
-        });
-    });
-}
-
-
 // Auto increment of the year in footer
 
 const copyrightYearRef = document.querySelector('#copyright-year')
