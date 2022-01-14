20dacf50f2027e6178399f717fc4881c1ed63238
sam-timmins <sam.timmins1@gmail.com>
2022-01-14 21:06:15 +0000

    Add timeout function for alerts

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,13 @@ $("#social-links-button").click(function () {
 })
 
 
+// Hides the alert after 3 seconds
+setTimeout(function () {
+        $("#msg").animate({'opacity':0}, 3000);
+    }, 2000)
+
+
+
 // Adds the active class to the active page in nav
 // Source - https://stackoverflow.com/questions/26819675/navbar-highlight-for-current-page/26819796
 $(function () {
