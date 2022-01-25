aaa2c49976ded5ae96f4809df6402ecb217dd6d8
sam-timmins <sam.timmins1@gmail.com>
2022-01-25 19:59:20 +0000

    Add path for search

--- issue/urls.py
+++ issue/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.Homepage.as_view(), name="home"),
     path("dashboard/", views.Dashboard.as_view(), name="dashboard"),
+    path("search-machines/", views.SearchMachines.as_view(), name="search-machines"),
     path("dashboard/create/machine/", views.CreateMachine.as_view(), name="create-machine"),
     path("dashboard/edit/machine/<int:pk>", views.EditMachine.as_view(), name="edit-machine"),
     path("dashboard/delete/machine/<int:pk>", views.delete_machine, name="delete-machine"),
