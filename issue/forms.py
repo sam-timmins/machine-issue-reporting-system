189d3c64a0f6834d8b3a93cc8d07c90c2957c728
sam-timmins <sam.timmins1@gmail.com>
2022-01-29 21:16:23 +0000

    Add EditStaffStatusForm

--- issue/forms.py
+++ issue/forms.py
@@ -7,4 +7,17 @@ from django.contrib.auth.forms import UserChangeForm
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-        fields = ('description',)
+        fields = (
+            'description',
+        )
+
+       
+class EditStaffStatusForm(UserChangeForm):
+
+    is_staff = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = (
+            'is_staff',
+        )
