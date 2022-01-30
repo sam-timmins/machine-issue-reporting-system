fdd65c1dfd1ab3c382fb8e826b8e56719e2b54fa
sam-timmins <sam.timmins1@gmail.com>
2022-01-30 14:18:16 +0000

    Add imports of env variables

--- accounts/views.py
+++ accounts/views.py
@@ -8,15 +8,22 @@ from django.contrib.messages.views import SuccessMessageMixin
 from .models import User
 from .forms import EditStaffStatusForm
 
-from env import (
-    UNIVERSITY_NAME,
-    FACEBOOK_LINK,
-    INSTAGRAM_LINK,
-    TWITTER_LINK,
-    MACHINE_CARDS_CURRENT_ISSUE_TEXT,
-    NO_ISSUES_MODAL_TITLE,
-    NO_ISSUES_TEXT
-)
+from pathlib import Path
+import os
+
+# Prevent error when hidden env.py is not found
+if os.path.isfile('env.py'):
+    import env
+
+
+UNIVERSITY_NAME = os.environ.get('UNIVERSITY_NAME')
+FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
+INSTAGRAM_LINK = os.environ.get('INSTAGRAM_LINK')
+FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
+TWITTER_LINK = os.environ.get('TWITTER_LINK')
+MACHINE_CARDS_CURRENT_ISSUE_TEXT = os.environ.get('MACHINE_CARDS_CURRENT_ISSUE_TEXT')
+NO_ISSUES_MODAL_TITLE = os.environ.get('NO_ISSUES_MODAL_TITLE')
+NO_ISSUES_TEXT = os.environ.get('NO_ISSUES_TEXT')
 
 
 class ViewAllUsers(generic.ListView):
