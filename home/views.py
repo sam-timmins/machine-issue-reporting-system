a5e4b6b8fc7eb3ab2d55ca84f521b34170a33d17
sam-timmins <sam.timmins1@gmail.com>
2022-02-02 19:27:51 +0000

    Add Homepage view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Homepage(TemplateView):
+    """
+    Views the homepage
+    """
+    template_name = 'index.html'
+
+    extra_context = {
+        'university_name': UNIVERSITY_NAME,
+        'facebook': FACEBOOK_LINK,
+        'instagram': INSTAGRAM_LINK,
+        'twitter': TWITTER_LINK,
+        }
