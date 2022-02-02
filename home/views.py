0ce3c5afa585bd2f685905cb3c3e348dd0237efd
sam-timmins <sam.timmins1@gmail.com>
2022-02-02 19:28:26 +0000

    Add environment variables

--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from pathlib import Path
+import os
+
+# Prevent error when hidden env.py is not found
+if os.path.isfile('env.py'):
+    import env
+
+
+UNIVERSITY_NAME = os.environ.get('UNIVERSITY_NAME')
+FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
+INSTAGRAM_LINK = os.environ.get('INSTAGRAM_LINK')
+FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
+TWITTER_LINK = os.environ.get('TWITTER_LINK')
+MACHINE_CARDS_CURRENT_ISSUE_TEXT = os.environ.get('MACHINE_CARDS_CURRENT_ISSUE_TEXT')
+NO_ISSUES_MODAL_TITLE = os.environ.get('NO_ISSUES_MODAL_TITLE')
+NO_ISSUES_TEXT = os.environ.get('NO_ISSUES_TEXT')
+
 
 class Homepage(TemplateView):
     """
