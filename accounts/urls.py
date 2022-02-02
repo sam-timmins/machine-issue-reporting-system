1eeab2e6fdedf3e7f1e04941dc833ba6700a1658
sam-timmins <sam.timmins1@gmail.com>
2022-02-02 21:12:39 +0000

    Add login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('login/', views.LoginViewCustom.as_view(), name='login'),
     path("user/profile/all-users/", views.ViewAllUsers.as_view(), name="all-users"),
     path("user/profile/edit/staff/status/<int:pk>", views.EditStaffStatus.as_view(), name="edit-staff-status"),
     path("user/edit/profile/", views.UserEditProfile.as_view(), name="edit-profile"),
