6641d22c5c8b8d08910bd7cfc60d00d364e99481
sam-timmins <sam.timmins1@gmail.com>
2022-02-02 19:23:45 +0000

    Remoe homepage url

--- issue/urls.py
+++ issue/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.Homepage.as_view(), name="home"),
     path("dashboard/", views.Dashboard.as_view(), name="dashboard"),
     path("search-machines/", views.SearchMachines.as_view(), name="search-machines"),
     path("dashboard/create/machine/", views.CreateMachine.as_view(), name="create-machine"),
