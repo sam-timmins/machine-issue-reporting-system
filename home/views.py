82522c0c1855a8da9ec612f4681a9f95f53bb82e
sam-timmins <sam.timmins1@gmail.com>
2022-02-05 15:09:46 +0000

    Change format for PEP8 validator

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
+from pathlib import Path
+import os
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from pathlib import Path
-import os
 
 # Prevent error when hidden env.py is not found
 if os.path.isfile('env.py'):
@@ -14,7 +15,8 @@ FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
 INSTAGRAM_LINK = os.environ.get('INSTAGRAM_LINK')
 FACEBOOK_LINK = os.environ.get('FACEBOOK_LINK')
 TWITTER_LINK = os.environ.get('TWITTER_LINK')
-MACHINE_CARDS_CURRENT_ISSUE_TEXT = os.environ.get('MACHINE_CARDS_CURRENT_ISSUE_TEXT')
+MACHINE_CARDS_CURRENT_ISSUE_TEXT = os.environ.get(
+    'MACHINE_CARDS_CURRENT_ISSUE_TEXT')
 NO_ISSUES_MODAL_TITLE = os.environ.get('NO_ISSUES_MODAL_TITLE')
 NO_ISSUES_TEXT = os.environ.get('NO_ISSUES_TEXT')
 
