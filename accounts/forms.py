8516d78b902d6f6326b2bfdc67468ee667a18e8e
sam-timmins <sam.timmins1@gmail.com>
2022-02-05 13:51:56 +0000

    Add docstring to EditStaffStatusForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,9 @@ from .models import User
 
 
 class EditStaffStatusForm(UserChangeForm):
-
+    """
+    Form that can edit the user's is_staff boolean status using the User model
+    """
     is_staff = forms.BooleanField(required=False)
 
     class Meta:
