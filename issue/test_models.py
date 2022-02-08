82eec417ef05e17656799310d957c2bc18b3950f
sam-timmins <sam.timmins1@gmail.com>
2022-02-08 20:45:38 +0000

    Add tests for models

---
+++ issue/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from .models import Machine, Issue
+
+
+class TestModels(TestCase):
+    """
+    Test models
+    """   
+    def test_machine_string_item_returns_string(self):
+        """
+        Test to check the machine string item returns a string
+        """
+        machine = Machine.objects.create(
+            name='Test Machine'
+        )
+
+        self.assertEqual(str(machine), 'Test Machine')
+
+    def test_issue_string_item_returns_string(self):
+        """
+        Test to check the issue string item returns a string
+        """
+        machine = Machine.objects.create(
+            name='Test Machine'
+        )
+
+        issue = Issue.objects.create(
+            machine=machine
+        )
+
+        self.assertEqual(str(issue), 'Test Machine')
