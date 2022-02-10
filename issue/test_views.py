aafa2d3c38d7df59e4ab92d02079c82f097b7857
sam-timmins <sam.timmins1@gmail.com>
2022-02-10 21:31:24 +0000

    Add test setUp

--- issue/test_views.py
+++ issue/test_views.py
@@ -1,2 +1,22 @@
 from django.test import TestCase
 from .models import Machine, Issue
+
+class TestViews(TestCase):
+    """
+    Test issue application views
+    """
+    def setUp(self):
+        machine_a = Machine(
+            name='test machine',
+            description='Test description',
+        )
+
+        self.machine_list = machine_list
+
+        issue_a = Issue(
+            machine=machine_a,
+            description='Test issue description'
+        )
+
+        issue_a.save()
+        self.issue_a = issue_a
